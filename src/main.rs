a028069b3cab8af8523c6a0f6c89aac424125bed
clemkeirua <clement@keiruaprod.fr>
2019-09-23 22:43:28 +0200

    added monitoring time to the headers

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 extern crate isahc;
 extern crate chrono;
+extern crate http;
 use chrono::prelude::*;
+use http::header::HeaderValue;
 
 fn main() {
     let start = Utc::now();
-    let response = isahc::head("https://httpbin.org/get").unwrap();
+    let mut response = isahc::head("https://www.evaneos.com").unwrap();
     let end = Utc::now();
+    let millis = end.timestamp_millis()-start.timestamp_millis();
+    let millis_str = format!("{}", millis);
+    response.headers_mut().insert("X-MONITHOR-TIME-ms", HeaderValue::from_str(&millis_str).unwrap());
     println!("{:#?}", response.headers());
-    println!("{}ms", end.timestamp_millis()-start.timestamp_millis());
 }
