72d975370160fdc3a54552335152907778751e7d
clemkeirua <clement@keiruaprod.fr>
2019-09-23 23:10:13 +0200

    format

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,49 @@
-extern crate isahc;
 extern crate chrono;
 extern crate http;
+extern crate isahc;
 extern crate structopt;
 use chrono::prelude::*;
 use http::header::{HeaderMap, HeaderValue};
 use http::StatusCode;
-use structopt::StructOpt;
 use std::fmt;
+use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "example", about = "An example of StructOpt usage.")]
 struct Opt {
     #[structopt(short = "u", long = "url")]
-    url: String
+    url: String,
 }
 
 struct MonitoringData<'a> {
-    response_time_ms:i64,
+    response_time_ms: i64,
     response_code: StatusCode,
-    headers: &'a HeaderMap<HeaderValue>
+    headers: &'a HeaderMap<HeaderValue>,
 }
 
 impl<'a> MonitoringData<'a> {
-    pub fn new(response_time_ms: i64, response_code: StatusCode, headers: &'a HeaderMap<HeaderValue>) -> MonitoringData {
-        MonitoringData{response_time_ms, response_code, headers}
+    pub fn new(
+        response_time_ms: i64,
+        response_code: StatusCode,
+        headers: &'a HeaderMap<HeaderValue>,
+    ) -> MonitoringData {
+        MonitoringData {
+            response_time_ms,
+            response_code,
+            headers,
+        }
     }
 }
 
 impl<'a> fmt::Debug for MonitoringData<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{{ response_time: {:?},\nresponse_code: {}\nheaders: {:#?} }}", self.response_time_ms, self.response_code.as_u16(), self.headers)
+        write!(
+            f,
+            "{{ response_time: {:?},\nresponse_code: {}\nheaders: {:#?} }}",
+            self.response_time_ms,
+            self.response_code.as_u16(),
+            self.headers
+        )
     }
 }
 
@@ -40,9 +54,11 @@ fn main() {
     let response = isahc::head(opt.url).unwrap();
     let end = Utc::now();
 
-    let millis = end.timestamp_millis()-start.timestamp_millis();
-    let result = MonitoringData::new(millis, response.status(), response.headers());
+    let result = MonitoringData::new(
+        end.timestamp_millis() - start.timestamp_millis(),
+        response.status(),
+        response.headers(),
+    );
 
-    /*let serialized = serde_json::to_string(&point).unwrap();*/
     println!("{:#?}", result);
 }
