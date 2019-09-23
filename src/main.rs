822a8fc8bc348c8521525225fa12995a37776758
clemkeirua <clement@keiruaprod.fr>
2019-09-23 23:06:40 +0200

    working draft

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ extern crate structopt;
 use chrono::prelude::*;
 use http::header::{HeaderMap, HeaderValue};
 use http::StatusCode;
-
 use structopt::StructOpt;
+use std::fmt;
 
 #[derive(Debug, StructOpt)]
 #[structopt(name = "example", about = "An example of StructOpt usage.")]
@@ -15,7 +15,6 @@ struct Opt {
     url: String
 }
 
-#[derive(Debug)]
 struct MonitoringData<'a> {
     response_time_ms:i64,
     response_code: StatusCode,
@@ -25,6 +24,12 @@ struct MonitoringData<'a> {
 impl<'a> MonitoringData<'a> {
     pub fn new(response_time_ms: i64, response_code: StatusCode, headers: &'a HeaderMap<HeaderValue>) -> MonitoringData {
         MonitoringData{response_time_ms, response_code, headers}
+    }
+}
+
+impl<'a> fmt::Debug for MonitoringData<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{{ response_time: {:?},\nresponse_code: {}\nheaders: {:#?} }}", self.response_time_ms, self.response_code.as_u16(), self.headers)
     }
 }
 
@@ -38,5 +43,6 @@ fn main() {
     let millis = end.timestamp_millis()-start.timestamp_millis();
     let result = MonitoringData::new(millis, response.status(), response.headers());
 
-    println!("{:?}", result);
+    /*let serialized = serde_json::to_string(&point).unwrap();*/
+    println!("{:#?}", result);
 }
