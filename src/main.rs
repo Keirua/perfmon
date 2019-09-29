837a986290cb48cb893acb746f069425bda1ff86
clemkeirua <clement@keiruaprod.fr>
2019-09-30 00:48:47 +0200

    added possibility to set the verb through CLI

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,45 @@ use structopt::StructOpt;
 mod monitoring_data;
 use monitoring_data::*;
 
+#[derive(Debug)]
+enum HttpVerb {
+    Get, 
+    Head
+}
+use std::str::FromStr;
+
+impl FromStr for HttpVerb {
+    type Err = std::convert::Infallible;
+    fn from_str(verb: &str) -> Result<Self, Self::Err> {
+        match verb {
+            "get" => Ok(HttpVerb::Get),
+            "head" => Ok(HttpVerb::Head),
+            _ => Ok(HttpVerb::Head),
+        }
+    }
+}
+
 #[derive(Debug, StructOpt)]
 #[structopt(name = "example", about = "An example of StructOpt usage.")]
 struct Opt {
     #[structopt(short = "u", long = "url")]
     url: String,
+    #[structopt(long = "verb", default_value = "get")]
+    verb: HttpVerb,
+}
+
+fn fetch_response(url: &str, verb: &HttpVerb) -> Result<Response<Body>, isahc::Error> {
+    match verb {
+        HttpVerb::Head => isahc::head(url),
+        HttpVerb::Get => isahc::get(url)
+    }
 }
 
 fn main() -> Result<(), isahc::Error> {
     let opt = Opt::from_args();
 
     let start = Utc::now();
-    let mut response = isahc::head(opt.url)?;
+    let mut response = fetch_response(&opt.url, &opt.verb)?;
     let end = Utc::now();
 
     let result = MonitoringData::new(
