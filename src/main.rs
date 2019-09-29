84017b7ee94993535702e2dc3dee322a9ce906d7
clemkeirua <clement@keiruaprod.fr>
2019-09-29 23:20:55 +0200

    added research of the body size whenever possible

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate http;
 extern crate isahc;
 extern crate structopt;
 use chrono::prelude::*;
+use isahc::prelude::*;
 use http::header::{HeaderMap, HeaderValue};
 use http::StatusCode;
 use std::fmt;
@@ -18,6 +19,7 @@ struct Opt {
 struct MonitoringData<'a> {
     response_time_ms: i64,
     response_code: StatusCode,
+    response_body_size_bytes: usize,
     headers: &'a HeaderMap<HeaderValue>,
 }
 
@@ -25,11 +27,13 @@ impl<'a> MonitoringData<'a> {
     pub fn new(
         response_time_ms: i64,
         response_code: StatusCode,
+        response_body_size_bytes: usize,
         headers: &'a HeaderMap<HeaderValue>,
     ) -> MonitoringData {
         MonitoringData {
             response_time_ms,
             response_code,
+            response_body_size_bytes,
             headers,
         }
     }
@@ -39,25 +43,27 @@ impl<'a> fmt::Debug for MonitoringData<'a> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
-            "{{ \"response_time\": {:?},\n\"response_code\": {}}}",
+            "{{ \"response_time\": {:?},\n\"response_code\": {} }}",
             self.response_time_ms,
             self.response_code.as_u16()
         )
     }
 }
 
-fn main() {
+fn main() -> Result<(), isahc::Error>{
     let opt = Opt::from_args();
 
     let start = Utc::now();
-    let response = isahc::head(opt.url).unwrap();
+    let mut response = isahc::head(opt.url)?;
     let end = Utc::now();
 
     let result = MonitoringData::new(
         end.timestamp_millis() - start.timestamp_millis(),
         response.status(),
+        response.text()?.len(),
         response.headers(),
     );
 
     println!("{:#?}", result);
+    Ok(())
 }
